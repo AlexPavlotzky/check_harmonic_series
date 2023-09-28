88413d30b7d46cae006f6a4c98d3493ac6c037c1
AlexPavlotzky <alexpavlotzky@gmail.com>
2023-09-28 00:08:50 -0300

    .

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ from check_harmonic_series import *
 from virgiliano import *
 
 if __name__ == "__main__":
-    instrumento = input("Bienvenido, por favor ingrese el registro de su sacabuche:\n"
-        "1. Sacabuche alto en D\n"
-        "2. Sacabuche tenor en A\n"
-        "3. Sacabuche bajo en A\n"
-        "4. Otro\n--> ")
+    instrumento = input("Benvenuto, scegli il tipo di trombone:\n"
+        "1. Trombone contralto in D\n"
+        "2. Trombone tenore in A\n"
+        "3. Trombone basso in D\n"
+        "4. Altro\n--> ")
     
-    if instrumento == "1":
+    if instrumento == "1" or instrumento == "3":
         nota_base = "D"
-    elif instrumento == "2" or instrumento == "3":
+    elif instrumento == "2":
         nota_base = "A"
     elif instrumento == "4":
-        nota_base = input("Ingrese la nota base del instrumento (C, C#/Db, D, D#/Eb, E, F, F#/Gb, G, G#/Ab, A, A#/Bb, B):\n-->  ")
+        nota_base = input("Inserisci la nota di base dello strumento (C, C#/Db, D, D#/Eb, E, F, F#/Gb, G, G#/Ab, A, A#/Bb, B):\n-->  ")
     generator = SerieArmonicaGenerator()
     serie_combinada = generator.mostrar_serie_por_posicion(nota_base)
     sacabuche = Virgiliano()
